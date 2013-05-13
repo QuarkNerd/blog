d0ca2c29328abd1aa2252291f4ccf068ed9ea8a8
Michael Orchard <morchard@scottlogic.co.uk>
2013-05-13 10:46:37 +0100

    Post counts
    Change-Id: I83b347d0297e8f182ce3197ee337168489693822

--- scripts/disqus-comment-count.js
+++ scripts/disqus-comment-count.js
@@ -1,33 +1,29 @@
 var disqus_shortname = 'scottlogic';
 
 var dq_publickey = "Aj9eMiGDAUD3UtxJyfrldMc9nhkIvzk5CmQ7eNK3iSdseO9seaatoLdn4SghoTlZ";
-var dq_urls = [];
+var dq_ids = [];
 
-// API Query
 (function() {
   jQuery('.dq-comment-count').each(function () {
-    dq_urls.push(jQuery(this).attr('data-disqus-identifier'));
+    dq_ids.push('ident:' + jQuery(this).attr('data-disqus-identifier'));
+    jQuery(this).html('0 comments');
   });
 
   jQuery.ajax({
     type: 'GET',
     url: 'https://disqus.com/api/3.0/threads/set.jsonp',
-    data: { api_key: dq_publickey, forum: disqus_shortname, thread: dq_urls },
+    data: { api_key: dq_publickey, forum: disqus_shortname, thread: dq_ids },
     cache: false,
     dataType: 'jsonp',
     success: function(result) {
       for(var i in result.response) {
         var num = result.response[i].posts;
         var txt = (num == 1) ? ' comment' : ' comments';
-        jQuery('div[data-disqus-identifier="' + result.response[i].link + '"]').html(num + txt);
+
+        for(var j in result.response[i].identifiers) {
+          jQuery('div[data-disqus-identifier="' + result.response[i].identifiers[j] + '"]').html(num + txt);
+        }
       }
     }
   });
-}());
-
-// Disqus automatic count generation
-(function () {
-  var s = document.createElement('script');
-  s.async = true; s.type = 'text/javascript'; s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
-  (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
 }());
