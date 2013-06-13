e719451e722f208994b9970551323eae5d870404
godds <godds@scottlogic.co.uk>
2013-06-13 11:31:43 +0100

    tweet button now sets up appropriate text for tweet

--- scripts/tweet.js
+++ scripts/tweet.js
@@ -4,19 +4,13 @@
 
     jQuery(".tweet").each(function() {
         var elem = jQuery(this),
-        // Use current page URL as default link
+            // Use current page URL as default link
             url = encodeURIComponent(elem.attr("data-url") || document.location.href),
-        // Use page title as default tweet message
-            text = elem.attr("data-text") || document.title;/*,
-            via = elem.attr("data-via") || "",
-            related = encodeURIComponent(elem.attr("data-related")) || "",
-            hashtags = encodeURIComponent(elem.attr("data-hashtags")) || "";*/
+            // Use page title as default tweet message
+            text = elem.attr("data-text") || document.title;
 
         // Set href to tweet page
         elem.attr({
-            /*href: TWEET_URL + "?hashtags=" + hashtags + "&original_referer=" +
-                encodeURIComponent(document.location.href) + "&related=" + related +
-                "&source=tweetbutton&text=" + text + "&url=" + url + "&via=" + via,*/
             href: TWEET_URL + "?source=tweetbutton&text=" + text + "&url=" + url,
             target: "_blank"
         });
