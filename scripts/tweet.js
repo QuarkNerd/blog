11cb419ecaa83ab33822cf0c7980337db23d1bb2
Colin Eberhardt <ceberhardt@scottlogic.co.uk>
2015-08-07 15:41:26 +0100

    Social icons / count tidyup
     + Twitter and disqus icons now have the same height
     + Removed some redundant repeated CSS
     + Twitter icons are no longer 'tweet' links
     + tweet / discuss counts are now vertically aligned properly
     + Tweet counts use the canonical URL so now work on forks / local

--- scripts/tweet.js
+++ scripts/tweet.js
@@ -5,17 +5,9 @@ function loadTweetCount() {
     jQuery(".tweet").each(function() {
         var elem = jQuery(this),
             // Use current page URL as default link
-            url = encodeURIComponent(elem.attr("data-url") || document.location.href),
-            // Use page title as default tweet message
-            text = elem.attr("data-text") || document.title;
+            url = encodeURIComponent(elem.attr("data-url") || document.location.href);
 
         if (elem.find(".count").is(':empty')) {
-            // Set href to tweet page
-            elem.attr({
-                href: TWEET_URL + "?source=tweetbutton&text=" + text + "&url=" + url,
-                target: "_blank"
-            });
-
             // Get count and set it as the inner HTML of .count
             jQuery.getJSON(API_URL + "?callback=?&url=" + url, function(data) {
                 elem.find(".count").html(data.count);
